ae2a75e9bd4cb132d4926d3563dbd831b36c541d
Tushar Karamchandani <tusharkaramchandani25@gmail.com>
2026-01-31 14:32:15 +0530

    Remove the example `helloWorld` Inngest function and API route, and add AI SDK dependencies.

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -1,9 +1,8 @@
-import { helloWorld } from "@/inngest/function";
 import { serve } from "inngest/next";
 import { inngest } from "../../../inngest/client";
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [helloWorld],
+  functions: [],
 });
